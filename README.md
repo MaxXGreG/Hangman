a6d3bb048513890ec44f79b4a3528da6d93fefcb
Max G <makgregoric@gmail.com>
2020-06-21 22:26:52 +0200

    Add underscores

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
-#A hangman app made with HTML,CSS and JavaScript
+###A hangman app made with HTML,CSS and JavaScript
 
 
-[x] Write the markup
-[x] Style the markup
-[] Generate random words
-[] Add guess lettter function
-[] Add guess word function
-[] Add reward and punishment
-[] Add game cycle 
+* [x] Write the markup
+* [x] Style the markup
+* [x] Generate random words
+* [x] Get underscores
+* [x] Display underscores
+* [] Add guess lettter function
+* [] Add guess word function
+* [] Add reward and punishment
+* [] Add guessed letters container
+* [] Add game cycle 
--- script.js
+++ script.js
@@ -3,4 +3,57 @@ let head = '<img src="/img/head.png" alt="head">'
 let body = '<img src="/img/headBody.png" alt="Body">'
 let legs = '<img src="/img/legs.png" alt="legs">'
 let hands = '<img src="/img/hands.png" alt="legs">'
+let words = ["checkbook",
+    "twister",
+    "pencil",
+    "glow", "stick",
+    "toe", "ring",
+    "thermometer",
+    "lamp",
+    "spoon",
+    "paint", "brush",
+    "food",
+    "cork",
+    "twezzers",
+    "puddle",
+    "pillow",
+    "television",
+    "radio",
+    "rug",
+    "chapter", "book",
+    "button",
+    "lamp", "shade",
+    "outlet",
+    "blouse",
+    "beef",
+    "needle",
+    "desk",
+    "plastic", "fork",
+    "photo", "album",
+    "tire", "swing",
+    "rubber", "duck",
+    "sand", "paper"
+]
 
+function getRandom() {
+    return Math.floor(Math.random()*30)
+}
+
+function startGame() {
+    let word = getUnderscore(words[getRandom()])
+    document.querySelector('#letters').innerHTML = word
+}
+
+function getUnderscore(cw) {
+    let word = cw.split("")
+    let underscores = ''
+    for (let i = 0; i<word.length; i++) {
+        underscores = underscores.concat("_ ")
+    }
+    return (underscores)
+}
+
+function letterGuess() {
+    let letter = document.querySelector('#letterGuess').value
+
+}
