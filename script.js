7e352e032f94ff41f712fceebff67d89801c4fe2
Max G <makgregoric@gmail.com>
2020-06-21 13:39:24 +0200

    Add basic markup

---
+++ script.js
@@ -0,0 +1,6 @@
+let rope = '<img src="/img/hangman.png" alt="rope"></img>'
+let head = '<img src="/img/head.png" alt="head">'
+let body = '<img src="/img/headBody.png" alt="Body">'
+let legs = '<img src="/img/legs.png" alt="legs">'
+let hands = '<img src="/img/hands.png" alt="legs">'
+
